16e881cb31771ce3b0d430e879345a3da98e4e0f
darikmohammed <r.m.darik@gmail.com>
2022-07-28 02:42:05 +0300

    Add function of Search

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Characters from '../Components/Characters';
 import Navbar from '../Components/Navbar';
@@ -8,6 +8,7 @@ import Container from './Styles/Home.style';
 
 function Home() {
   const dispatch = useDispatch();
+  const [search, setSearch] = useState('');
   const characters = useSelector((state) => state.Disney.characters);
   useEffect(() => {
     dispatch(GetDisney());
@@ -15,17 +16,34 @@ function Home() {
   return (
     <div>
       <Navbar heading="Disney Characters" />
+      <input
+        type="text"
+        placeholder="Search ...."
+        onChange={(event) => {
+          setSearch(event.target.value);
+        }}
+      />
       <Container>
-        {characters.map((character) => (
-          <Characters
-            /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
-            key={character._id}
-            name={character.name}
-            id={character._id}
-            image={character.imageUrl}
-            tvShows={character.tvShows.length}
-          />
-        ))}
+        {characters
+          .filter((value) => {
+            if (!value) {
+              return value;
+            }
+            if (value.name.toLowerCase().includes(search.toLowerCase())) {
+              return value;
+            }
+            return null;
+          })
+          .map((character) => (
+            <Characters
+              /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
+              key={character._id}
+              name={character.name}
+              id={character._id}
+              image={character.imageUrl}
+              tvShows={character.tvShows.length}
+            />
+          ))}
       </Container>
     </div>
   );
